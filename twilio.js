5e578f995b7c68bb4a27709bf3ace915c5ec6b24
LOZORD <ljrudberg@gmail.com>
2015-11-21 18:41:45 -0600

    Added web sockets for phone -> server -> game communication

--- twilio.js
+++ twilio.js
@@ -1,3 +1,4 @@
+/*
 var client = require('twilio')('A_SID', 'A_TKN');
 var express = require('express');
 
@@ -13,4 +14,45 @@ var server = app.listen(3000, function() {
   var port = server.address().port;
 
   console.log('space-cats listening at host ', host, 'port ', port);
+});
+
+//var WebSocket = require('ws');
+//var ws = new WebSocket('ws://
+*/
+
+var server            = require('http').createServer(),
+    url               = require('url'),
+    WebSocketServer   = require('ws').Server,
+    wss               = new WebSocketServer({ server: server }),
+    express           = require('express'),
+    app               = express(),
+    port              = 3000;
+
+app.get('/message', function(req, res) {
+  res.send('<Response><Message>Thanks for letting us know.</Message></Response>');
+  if (req.query.Body) {
+    console.log(req.query.Body);
+  }
+});
+
+var incoming = function(message) {
+  console.log('received: %s', message);
+};
+
+var connection = function(ws) {
+  var loc = url.parse(ws.upgradeReq.url, true);
+
+  ws.on('message', incoming);
+
+  ws.send('something!');
+};
+
+wss.on('connection', connection);
+
+server.on('request', app);
+server.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('SPACE CATS listening at host: %s, port: %s', host, port);
 });
