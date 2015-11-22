0c3585375441aab2714e59ba1e75848b4aeb28ce
LOZORD <ljrudberg@gmail.com>
2015-11-21 20:54:45 -0600

    It's janky, but it works

--- js/twilio.js
+++ js/twilio.js
@@ -1,25 +1,3 @@
-/*
-var client = require('twilio')('A_SID', 'A_TKN');
-var express = require('express');
-
-var app = express();
-
-app.get('/message', function(req, res) {
-  res.send('<Response><Message>Thanks for letting us know.</Message></Response>');
-  console.log(req.query.Body);
-});
-
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('space-cats listening at host ', host, 'port ', port);
-});
-
-//var WebSocket = require('ws');
-//var ws = new WebSocket('ws://
-*/
-
 var server            = require('http').createServer(),
     url               = require('url'),
     WebSocketServer   = require('ws').Server,
@@ -32,36 +10,32 @@ var textMessageResponse =
   '<Response><Message>Thanks for letting us know.</Message></Response>';
 
 var incoming = function(message) {
-  console.log('received: %s', message);
+  console.log('RECEIVED: %s', message);
 };
-
-//app.get('/message', replyToTextAndRespondToFrontEndWithSocket(null));
 
 var connection = function(ws) {
   var loc = url.parse(ws.upgradeReq.url, true);
 
   ws.on('message', incoming);
 
-  //replyToTextAndRespondToFrontEndWithSocket(ws);
-
   // what is sent to the front-end
-  ws.send('something!');
+  //ws.send('something!');
 
   app.get('/message', function(req, res) {
     //res.send('<Response><Message>Thanks for letting us know.</Message></Response>');
-    /*
     res.send(textMessageResponse);
-    if (req.query.Body) {
-      console.log(req.query.Body);
+    var textMessageData = req.query.Body;
+    if (textMessageData) {
+      console.log(textMessageData);
       if (ws && ws.send) {
-        ws.send(req.query.Body); // send to the client, hopefully
+        ws.send(textMessageData);
       }
     }
-    */
-    ws.send(req.query.Body);
+    //ws.send(req.query.Body);
   });
 };
 
+/*
 app.get('/message', function(req, res) {
   res.send('<Response><Message>Thanks for letting us know.</Message></Response>');
   if (req.query.Body) {
@@ -70,9 +44,10 @@ app.get('/message', function(req, res) {
     if (ws && ws.send) {
       ws.send(req.query.Body); // send to the client, hopefully
     }
-    */
+    *-/
   }
 });
+*/
 
 function replyToTextAndRespondToFrontEndWithSocket(ws) {
   return function(req, res) {
